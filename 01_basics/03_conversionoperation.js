2f2def313759118de7ae59ab47089b3ad0c1022a
PrafullYadav35 <147786594+PrafullYadav35@users.noreply.github.com>
2023-10-14 18:41:35 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,6 @@
+//agar string first hai to sbhi ko string me treat kiya jayega agar string last me hai to phle jo conversion hai vo ho jayega
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(+true)
+console.log(+"")
