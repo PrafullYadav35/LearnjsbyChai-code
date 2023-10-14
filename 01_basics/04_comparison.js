9f58591f748454478bb2504a71487d91609b2d09
PrafullYadav35 <147786594+PrafullYadav35@users.noreply.github.com>
2023-10-14 19:11:34 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,6 @@
+// js me comparison (>,>=,<,,) aur equality (==) check dono alag alag chize hai
+// valid comparison hmesha same data types ka krna chahiye
+
+console.log(null>0)
+console.log(null == 0)
+console.log(null >= )
