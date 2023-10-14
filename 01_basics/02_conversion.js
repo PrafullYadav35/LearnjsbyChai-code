53be4a91bbfe8a7ab47a90bb8078d43e894d60ed
PrafullYadav35 <147786594+PrafullYadav35@users.noreply.github.com>
2023-10-14 03:57:20 +0000

    conversion

---
+++ 01_basics/02_conversion.js
@@ -0,0 +1,8 @@
+let someNumber = 223
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+let btrue= 1
+let boolean= Boolean(btrue)
+console.log(boolean)
