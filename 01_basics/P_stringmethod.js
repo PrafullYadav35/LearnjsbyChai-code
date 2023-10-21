c89b40f19134cbeb1196797227d60a80f12e0ad4
PrafullYadav35 <147786594+PrafullYadav35@users.noreply.github.com>
2023-10-21 19:53:04 +0000

    hi

--- 01_basics/P_stringmethod.js
+++ 01_basics/P_stringmethod.js
@@ -1,3 +1,26 @@
 // practise string methods 
 let intro = `i am Prafull kumar yadav from ghazipur. i am living in lucknow`;
-console.log(intro.includes("ghazipur"));
+// return true is value match
+// console.log(intro.includes("Lucknow"));
+
+//match method
+
+// console.log(intro.match('i am living'));
+
+
+// eval ()  allows to written code in string will be evaluted
+// console.log(eval("\n5+10*2"));
+
+//replace()
+let parichay = intro.replace("lucknow","Ghazipur");
+// console.log(parichay);
+
+// search() search for the pattern in a string and return index position of match
+console.log(intro.search("yadav"));
+
+// escape()
+let schar = escape("i am founder of _");
+// console.log(schar);
+
+let password = encodeURI("I am billionre ");
+console.log(password);
