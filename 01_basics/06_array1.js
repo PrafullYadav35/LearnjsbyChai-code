f629e58b421583038c262ae60f3675adaab9749c
PrafullYadav35 <147786594+PrafullYadav35@users.noreply.github.com>
2023-10-27 13:24:58 +0000

    Array Methods

--- 01_basics/06_array1.js
+++ 01_basics/06_array1.js
@@ -7,9 +7,17 @@ const myArray =["Prafull","Shivam","Rohit","Sengar","Pratik"];
 // myArray.pop();
 // console.log(myArray);
 
-myArray.shift();
-console.log(myArray);
-myArray.unshift('Abhisek Yadav');           //unshift() method adds a new element to an array (at the beginning)
-console.log(myArray);
+// myArray.shift();
+// console.log(myArray);
+// myArray.unshift('Abhisek Yadav');           //unshift() method adds a new element to an array (at the beginning)
+// console.log(myArray);
 
+// console.log(myArray.includes(19));  // check rta array me hai element
+// console.log(myArray.indexOf(0));
 
+// console.log(myArray.slice(2,3));
+
+// console.log(myArray.splice(2,3));
+
+const newArr = myArray.join();
+console.log(newArr);
