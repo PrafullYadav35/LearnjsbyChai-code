f778fe904ad7082047d603c6fbf967fd1cabf28c
PrafullYadav35 <147786594+PrafullYadav35@users.noreply.github.com>
2023-10-27 11:05:24 +0000

    Array Methods

---
+++ 01_basics/06_array1.js
@@ -0,0 +1,15 @@
+const myArray =["Prafull","Shivam","Rohit","Sengar","Pratik"];
+// console.log(myArray);
+// myArray.push(35);
+// console.log(myArray);          //push () & pop()
+// myArray.pop();
+// console.log(myArray);
+// myArray.pop();
+// console.log(myArray);
+
+myArray.shift();
+console.log(myArray);
+myArray.unshift('Abhisek Yadav');           //unshift() method adds a new element to an array (at the beginning)
+console.log(myArray);
+
+
