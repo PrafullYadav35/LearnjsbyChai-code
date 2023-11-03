e1a407920ec0a4e08e3eac81eefd7468f282635e
PrafullYadav35 <147786594+PrafullYadav35@users.noreply.github.com>
2023-11-03 09:08:36 +0000

    array canacatenation and other methods

--- 01_basics/06_array2.js
+++ 01_basics/06_array2.js
@@ -1,27 +1,29 @@
 const class_hewett= ["Shivam srivastwa ","Abhisek Yadav", "Chetan ","PratikmSahoo"];
 const class_abic = ["Akash Singh","Ahisek","Sarvagya","Sonal Singhal"];
-const new1 =  class_hewett.push(class_abic);  //cocatation of array
+const new1 = class_hewett.push(class_abic);  //cocatation of array
 // console.log(class_hewett);
 
-let newArr2 = class_hewett.concat(class_abic);
+// let newArr2 = class_hewett.concat(class_abic);
 // console.log("newArr2 is  "+newArr2);
-// console.log("class Hewett  " + newArr2);
+// console.log("class Hewett  " + class_hewett);
 
-//concat troght spread operator
+//create a new array throgh spread operator
 const all_friend = [...class_hewett,...class_abic];
 // console.log(all_friend);
 
 //ek array ke element ko dusre Array me dalna 
-// const arr_flat = class_hewett.flat(class_abic);
-// let  arr_flat = arr_flat.flat(class_abic);
-// console.log(arr_flat);
+console.log(class_hewett);
+const arr_flat = class_hewett.flat();
+console.log(arr_flat);
 
 // console.log(Array.isArray(class_abic)); //check array hai ya nhi
 
 // console.log(Array.isArray(newArr2));
-console.log(Array.from("Prafull Kumar Yadav"));
-console.log(Array.from({name:"Surya DADA"}));  //intresting // isko btaya nhi kis type ka arry bnana hai
+// console.log(Array.from("Prafull Kumar Yadav"));
+// console.log(Array.from({name:"Surya DADA"}));  //intresting // isko btaya nhi kis type ka arry bnana hai
 
-console.log(Array.of("PINKI","MINKI ","sONI","sINKI"));  //
+// console.log(Array.of("PINKI","MINKI ","sONI","sINKI"));
+
+//
 
 // aur pdie  Array.is(), Array.from(), Array.of( )  ke bare me
