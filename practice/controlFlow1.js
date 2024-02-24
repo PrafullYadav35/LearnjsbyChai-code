a844dfdfdc4a3a392121a5511689f7900e2b5272
Prafull Kumar yadav <147786594+PrafullYadav35@users.noreply.github.com>
2024-02-24 12:20:45 +0000

    switch statement

---
+++ practice/controlFlow1.js
@@ -0,0 +1,56 @@
+//switch statements
+//JavaScript switch statement evaluates an expression. 
+//The expression’s value is compared with the values of each case in the structure. 
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+var month= "jan";
+switch(month){
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;  
+    case "apr":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break; 
+    case "jun":
+        console.log("June");
+        break; 
+    case "july":
+        console.log("July");
+        break;
+    case "aug":
+        console.log("August");
+        break;  
+    case "sept":
+        console.log("September");
+        break;  
+    case "oct":
+        console.log("October");
+        break;
+    case "nov":
+        console.log("November");
+        break;  
+    case "dec":
+        console.log("December");
+        break;  
+    default:
+         console.log("not entered month")
+                           
+}
