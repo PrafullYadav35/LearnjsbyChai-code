cc8b1c16f28d38ee7ea067ae5dfbfbb49a41c885
Prafull Kumar yadav <147786594+PrafullYadav35@users.noreply.github.com>
2024-02-24 15:55:17 +0000

    while loop

---
+++ practice/loop_while.js
@@ -0,0 +1,19 @@
+let i = 1;
+while(i<=10){
+    //console.log(i*5);
+    i= i+1;
+}
+
+let fruits =["apple","banana","berry","grapes","mango",'kiwi'];
+let index =0;
+while(index< fruits.length){
+    //console.log(fruits[index]);
+    index++;
+}
+
+//do while loop 
+let j=0;
+do{
+ console.log(fruits[j]);
+ j++;   
+}while(j>fruits.length);
