5147b40cb4cd303a5bb83163f12bbe9cc2451ff3
Prafull Kumar yadav <147786594+PrafullYadav35@users.noreply.github.com>
2024-02-27 19:04:36 +0000

    Date in js

--- 01_basics/05_date.js
+++ 01_basics/05_date.js
@@ -1,5 +1,32 @@
 let myDate = new Date();
 // console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toLocaleString());
+
+
+//new Date(year,month,day)
+//let myCreatedDate = new Date(2024,0,24);  //0- january & 11 - December
+
+
+//new Date(year,month,day,hours,minutes)
+//let myCreatedDate = new Date(2024,0,24,5,3);
+
+
+//new Date(date string)
+let myCreatedDate = new Date("2024-02-28");
+
+//let myCreatedDate =  new Date("28-02-2024"); //output:  invalid date 
+
+console.log(myCreatedDate.toLocaleString());
+// toLocaleString() method date & time btata  hai kewal
+
+let myTimeStamp = Date.now();
+//console.log(myTimeStamp);
+//console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now())/1000);
+
+let newDate= new Date();
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
